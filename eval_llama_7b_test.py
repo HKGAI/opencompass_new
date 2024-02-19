5d13a2f835e64cb543497851b28564501e41eba5
Chaseldot <chaselyeung@gmail.com>
2024-02-19 22:14:46 +0800

    new opencompass

--- eval_llama_7b_test.py
+++ eval_llama_7b_test.py
@@ -3,33 +3,33 @@ from mmengine.config import read_base
 with read_base():
     ########################DATASET##################
     # Standard Benchmarks
-    from .configs.datasets.SuperGLUE_BoolQ.SuperGLUE_BoolQ_ppl_314797 import BoolQ_datasets
-    from .configs.datasets.piqa.piqa_ppl_0cfff2 import piqa_datasets
-    from .configs.datasets.siqa.siqa_ppl_e8d8c5 import siqa_datasets
-    from .configs.datasets.hellaswag.hellaswag_ppl_a6e128 import hellaswag_datasets
-    from .configs.datasets.winogrande.winogrande_ppl_55a66e import winogrande_datasets
-    from .configs.datasets.ARC_e.ARC_e_ppl_2ef631 import ARC_e_datasets
-    from .configs.datasets.ARC_c.ARC_c_ppl_2ef631 import ARC_c_datasets
-    from .configs.datasets.obqa.obqa_ppl_6aac9e import obqa_datasets
-    from .configs.datasets.commonsenseqa.commonsenseqa_ppl_e51e32 import commonsenseqa_datasets
-    from .configs.datasets.mmlu.mmlu_ppl_ac766d import mmlu_datasets
+    from .configs.datasets.SuperGLUE_BoolQ.SuperGLUE_BoolQ_ppl import BoolQ_datasets
+    from .configs.datasets.piqa.piqa_ppl import piqa_datasets
+    from .configs.datasets.siqa.siqa_ppl import siqa_datasets
+    from .configs.datasets.hellaswag.hellaswag_ppl import hellaswag_datasets
+    from .configs.datasets.winogrande.winogrande_ppl import winogrande_datasets
+    from .configs.datasets.ARC_e.ARC_e_ppl import ARC_e_datasets
+    from .configs.datasets.ARC_c.ARC_c_ppl import ARC_c_datasets
+    from .configs.datasets.obqa.obqa_ppl import obqa_datasets
+    from .configs.datasets.commonsenseqa.commonsenseqa_ppl import commonsenseqa_datasets
+    from .configs.datasets.mmlu.mmlu_ppl import mmlu_datasets
     # Code Generation
-    from .configs.datasets.humaneval.humaneval_gen_a82cae import humaneval_datasets
-    from .configs.datasets.mbpp.mbpp_gen_1e1056 import mbpp_datasets
+    from .configs.datasets.humaneval.humaneval_gen import humaneval_datasets
+    from .configs.datasets.mbpp.mbpp_gen import mbpp_datasets
     # World Knowledge           need NaturalQuestions
-    from .configs.datasets.nq.nq_gen_0356ec import nq_datasets
-    from .configs.datasets.triviaqa.triviaqa_gen_0356ec import triviaqa_datasets
+    from .configs.datasets.nq.nq_gen import nq_datasets
+    from .configs.datasets.triviaqa.triviaqa_gen import triviaqa_datasets
     
     # Reading Comprehension       need QUAC
-    from .configs.datasets.squad20.squad20_gen_1710bc import squad20_datasets
+    from .configs.datasets.squad20.squad20_gen import squad20_datasets
 
     # Exams
-    from .configs.datasets.gsm8k.gsm8k_gen_1d7fe4 import gsm8k_datasets
-    from .configs.datasets.math.math_gen_265cce import math_datasets
-    from .configs.datasets.TheoremQA.TheoremQA_gen_ef26ca import TheoremQA_datasets
+    from .configs.datasets.gsm8k.gsm8k_gen import gsm8k_datasets
+    from .configs.datasets.math.math_gen import math_datasets
+    from .configs.datasets.TheoremQA.TheoremQA_gen import TheoremQA_datasets
     # ceval and cmmlu
-    from .configs.datasets.ceval.ceval_ppl_578f8d import ceval_datasets
-    from .configs.datasets.cmmlu.cmmlu_ppl_8b9c76 import cmmlu_datasets
+    from .configs.datasets.ceval.ceval_ppl import ceval_datasets
+    from .configs.datasets.cmmlu.cmmlu_ppl import cmmlu_datasets
 
     #########################MODEL###################
     from .hf_llama_7b import models
@@ -39,6 +39,6 @@ with read_base():
 
 datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
 
-models = [models]
+models = models
 
 summarizer = dict(dataset_abbrs=dataset_abbrs, summary_groups=summary_groups, )
--- hf_llama_7b.py
+++ hf_llama_7b.py
@@ -1,22 +1,48 @@
 from opencompass.models import HuggingFaceCausalLM
 
+# 支持多个模型一起测评
+models = [
+    {
+        'type': 'HuggingFaceCausalLM',
+        'abbr': 'exp2.1/1283.46B',
+        'path': '/workspace/code/opencompass/checkpoints/hkg_7b_nl_tp1_pp1_mb1_gb512_gas2/exp2.1/hf_ckpt/1283.46B',
+        'tokenizer_path': '/workspace/code/Megatron-LM/hkgai/hf/hkg_amber_hf',
+        'tokenizer_kwargs': {
+            'padding_side': 'left',
+            'truncation_side': 'left',
+            'use_fast': False,
+            'trust_remote_code': True,
+        },
+        'max_out_len': 100,
+        'max_seq_len': 4096,
+        'batch_size': 16,
+        'model_kwargs': {
+            'device_map': 'auto', 
+            'trust_remote_code': True,
+        },
+        'batch_padding': False,
+        'run_cfg': {'num_gpus': 1, 'num_procs': 1},
+    },
+    # {
+    #     'type': 'HuggingFaceCausalLM',
+    #     'abbr': 'slimpajama/hf_096000',
+    #     'path': '/workspace/code/hf_ckpt/slimpajama/hf_096000',
+    #     'tokenizer_path': 'baichuan-inc/Baichuan2-7B-Base',
+    #     'tokenizer_kwargs': {
+    #         'padding_side': 'left',
+    #         'truncation_side': 'left',
+    #         'use_fast': False,
+    #         'trust_remote_code': True,
+    #     },
+    #     'max_out_len': 100,
+    #     'max_seq_len': 2048,
+    #     'batch_size': 16,
+    #     'model_kwargs': {
+    #         'device_map': 'auto', 
+    #         'trust_remote_code': True,
+    #     },
+    #     'batch_padding': False,
+    #     'run_cfg': {'num_gpus': 1, 'num_procs': 1},
+    # },
+]
 
-models = dict(
-            type=HuggingFaceCausalLM,
-            abbr='hf_llama-7b',
-            path='huggyllama/llama-7b',
-            tokenizer_path='huggyllama/llama-7b',
-            tokenizer_kwargs=dict(padding_side='left',
-                                truncation_side='left',
-                                use_fast=False,
-                                trust_remote_code=True,
-                                ),
-            max_out_len=100,
-            max_seq_len=2048,
-            batch_size=16,
-            model_kwargs=dict(device_map='auto', 
-                            trust_remote_code=True,
-                            ),
-            batch_padding=False, # if false, inference with for-loop without batch padding
-            run_cfg=dict(num_gpus=1, num_procs=1),
-        )
